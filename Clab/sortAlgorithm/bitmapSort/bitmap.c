1f94b26e32e35923841cbcc18e6e8c266d559f9d
Xu Di <xudifsd@gmail.com>
2011-03-24 20:28:43 +0800

    stop using struct * in bitmap sort instead using & operator

--- Clab/sortAlgorithm/bitmapSort/bitmap.c
+++ Clab/sortAlgorithm/bitmapSort/bitmap.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include "bitmap.h"
 
+#define ONE (1 << 1)
+#define TWO (1 << 2)
+#define THREE (1 << 3)
+#define FOUR (1 << 4)
+#define FIVE (1 << 5)
+#define SIX (1 << 6)
+#define SEVEN (1 << 7)
+#define EIGHT (1 << 8)
+
 #define MAX 1048576*2
 
 static char repository[MAX];	//bigest number can store is MAX*CHAR_BIT-1
@@ -10,17 +19,6 @@ static char repository[MAX];	//bigest number can store is MAX*CHAR_BIT-1
 static char *byteOffset = repository;
 
 static unsigned short bitOffset = 0;
-
-struct field {
-	unsigned ONE : 1;
-	unsigned TWO : 1;
-	unsigned THREE : 1;
-	unsigned FOUR : 1;
-	unsigned FIVE : 1;
-	unsigned SIX : 1;
-	unsigned SEVEN : 1;
-	unsigned EIGHT : 1;
-};	//one byte
 
 extern void initRepository(){
 	memset(&repository, 0, sizeof(repository));
@@ -38,7 +36,6 @@ extern int add(int number){	//0 for success
 }
 
 extern int getint(void){	//return EOF indicate it has all returned
-	struct field * result;
 	for (;;){
 		if (bitOffset == 8){
 			byteOffset++;
@@ -47,59 +44,58 @@ extern int getint(void){	//return EOF indicate it has all returned
 		if (byteOffset == (repository + MAX))
 			return EOF;
 
-		result = byteOffset;
 		switch (bitOffset){
 			case 0 :
-				if (result->ONE == 0){
+				if (!(*byteOffset & ONE)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 1 :
-				if (result->TWO == 0){
+				if (!(*byteOffset & ONE)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 2 :
-				if (result->THREE == 0){
+				if (!(*byteOffset & TWO)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 3 :
-				if (result->FOUR == 0){
+				if (!(*byteOffset & THREE)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 4 :
-				if (result->FIVE == 0){
+				if (!(*byteOffset & FOUR)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 5 :
-				if (result->SIX == 0){
+				if (!(*byteOffset & FIVE)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 6 :
-				if (result->SEVEN == 0){
+				if (!(*byteOffset & SIX)){
 					bitOffset++;
 					continue;
 				}
 				else
 					return (((byteOffset - repository) * 8) + (bitOffset++));
 			case 7 :
-				if (result->EIGHT == 0){
+				if (!(*byteOffset & SEVEN)){
 					bitOffset++;
 					continue;
 				}
