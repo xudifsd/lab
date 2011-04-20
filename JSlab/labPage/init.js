d789c904080d6c5d4e4cd32705a8cf85d2f933ae
Xu Di <xudifsd@gmail.com>
2011-04-20 15:18:44 +0800

    use with statement to reduce characters

--- JSlab/labPage/init.js
+++ JSlab/labPage/init.js
@@ -1,17 +1,21 @@
 //Date : 2011-04-19 Author : xudifsd
 //this is a example to use right way of inputcheck and compatible
 var init = function(){
-	compatible.addcheckevent('textarea1','1','3','textarea1','tiparea1');	//this textarea don't lock submitButton
-	compatible.addcheckevent('textarea2','1','3','textarea2','tiparea2','submitButton');
-	compatible.addcheckevent('textarea3','1','3','textarea3','tiparea3','submitButton');
-	compatible.addcheckevent('textarea4','1','20','textarea4','tiparea4','submitButton');
+	with (compatible){
+		addcheckevent('textarea1','1','3','textarea1','tiparea1');	//this textarea don't lock submitButton
+		addcheckevent('textarea2','1','3','textarea2','tiparea2','submitButton');
+		addcheckevent('textarea3','1','3','textarea3','tiparea3','submitButton');
+		addcheckevent('textarea4','1','20','textarea4','tiparea4','submitButton');
+	}
 
 	inputcheck.initlockbutton('submitButton', 'textarea2', 'textarea3', 'textarea4');
 	//for ie
-	if (compatible.isie){
-		compatible.cleartiparea('tiparea1');
-		compatible.cleartiparea('tiparea2');
-		compatible.cleartiparea('tiparea3');
-		compatible.cleartiparea('tiparea4');
+	with (compatible){
+		if (isie){
+			cleartiparea('tiparea1');
+			cleartiparea('tiparea2');
+			cleartiparea('tiparea3');
+			cleartiparea('tiparea4');
+		}
 	}
 }
