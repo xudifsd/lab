4d7d53a965a08df94686c16560691852d99580e7
Xu Di <xudifsd@gmail.com>
2011-12-30 11:15:14 +0800

    write a util for unit test

---
+++ Schemelab/unit-test.scm
@@ -0,0 +1,42 @@
+;; Author : xudifsd, Date : 2011-12-30
+;; This is the utils for unit test
+;;
+;; case is a list whose elements also are list which used to applied to pro
+;; pro is a user specified procedure to test if case is passed the test,
+;; if pro returns true, case is passed, otherwise case is failed.
+
+(define (unit-test pro case)
+ (define (empty-case? case)
+  (null? case))
+ (define (first-case case)
+  (car case))
+ (define (next-case case)
+  (cdr case))
+ (define (test-success)
+  (display "unit-test passed")
+  (newline))
+ (define (report-failure case)
+  (display "unit-test fail at: ")
+  (display case)
+  (newline))
+
+ (if (empty-case? case)
+  (test-success)
+  (if (apply pro (first-case case))
+   (unit-test pro (next-case case))
+   (begin
+	(report-failure (first-case case))
+	(unit-test pro (next-case case))))))
+
+;; add new case to acc
+(define (add-case case acc)
+ (cons case acc))
+
+(define (init-case . cases)
+ (define (iter cases acc)
+  (if (null? cases)
+   empty-case
+   (iter (cdr cases) (cons (car cases) acc))))
+ (iter cases empty-case))
+
+(define empty-case '())
