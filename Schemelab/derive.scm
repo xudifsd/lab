5083478d906a8020650f4ce93e413a52401b93fd
Xu Di <xudifsd@gmail.com>
2012-09-10 22:39:14 +0800

    implemented sin, cos, tan, cot in derive and algebre-eval, we can now use these symbol in these procedure

--- Schemelab/derive.scm
+++ Schemelab/derive.scm
@@ -71,6 +71,18 @@
 (define (log? expression)
   (is-operator? expression 'log))
 
+(define (sin? expression)
+  (is-operator? expression 'sin))
+
+(define (cos? expression)
+  (is-operator? expression 'cos))
+
+(define (tan? expression)
+  (is-operator? expression 'tan))
+
+(define (cot? expression)
+  (is-operator? expression 'cot))
+
 (define (make-add a b)
   ;; FIXME
   ;; 1. merge it if a, b is both add expression
@@ -133,6 +145,18 @@
         (else
           (list 'log a b))))
 
+(define (make-sin a)
+  (list 'sin a))
+
+(define (make-cos a)
+  (list 'cos a))
+
+(define (make-tan a)
+  (list 'tan a))
+
+(define (make-cot a)
+  (list 'cot a))
+
 (define (derive expression variable)
   ;; derive function expression written in lisp, ie
   ;; (derive '(+ (* x x) x) 'x) ==> '(+ (* 2 x) 1)
@@ -143,8 +167,12 @@
   ;; 4. div
   ;; 5. exp
   ;; 6. log
-  ;; We preserve keywords: log e pi cos sin tan, so they can't be used as
-  ;; variable
+  ;; 7. sin
+  ;; 8. cos
+  ;; 9. tan
+  ;; 10. cot
+  ;; We preserve keywords: log e pi cos sin cos tan cot,
+  ;; so they can't be used as variable
   
   (cond
    ((or
@@ -153,29 +181,34 @@
      (eq? variable 'pi)
      (eq? variable 'cos)
      (eq? variable 'sin)
-     (eq? variable 'tan)) (print variable " is preserved word"))
+     (eq? variable 'cos)
+     (eq? variable 'tan)
+     (eq? variable 'cot)) (print variable " is preserved word"))
+
    ((constant? expression) 0)
+
    ((variable? expression)
     (if (same-variable? expression variable) 1 0))
    ((or (not (list? expression)) (not (variable? variable)))
     (print "Error expression " expression " is not a list" #\newline
            "or variable " variable " is not a symbol"))
+
    ;; from now on expression can only be list
    ((add? expression)
     (make-add (derive (first-argument expression) variable)
               (derive (rest-argument expression) variable)))
-   
+
    ((sub? expression)
     (make-sub (derive (first-argument expression) variable)
               (derive (sub-rest-argument expression) variable)))
-   
+
    ((mul? expression)
     (make-add
      (make-mul (derive (first-argument expression) variable)
                (rest-argument expression))
      (make-mul (derive (rest-argument expression) variable)
                (first-argument expression))))
-   
+
    ((div? expression)
     (make-div
      (make-sub (make-mul (derive (first-argument expression) variable)
@@ -183,7 +216,7 @@
                (make-mul (first-argument expression)
                          (derive (rest-argument expression) variable)))
      (make-exp (rest-argument expression) 2)))
-   
+
    ((exp? expression)
     (assert '(= (length expression) 3));;make sure it contains only two args
     (make-mul
@@ -194,7 +227,7 @@
       (make-mul (derive (first-argument expression) variable)
                 (make-div (rest-argument expression)
                           (first-argument expression))))))
-   
+
    ((log? expression)
     (assert '(= (length expression) 3));;make sure it contains only two args
     (make-div
@@ -210,7 +243,31 @@
         (derive (first-argument expression) variable)
         (make-log 'e (rest-argument expression)))))
      (make-exp (make-log 'e (first-argument expression)) 2)))
-   
+
+   ((sin? expression)
+    (assert '(= (length expression) 2))
+    (make-mul
+     (derive (first-argument expression) variable)
+     (make-cos (first-argument expression))))
+
+   ((cos? expression)
+    (assert '(= (length expression) 2))
+    (make-sub 0 (make-mul
+                 (derive (first-argument expression) variable)
+                 (make-sin (first-argument expression)))))
+
+   ((tan? expression)
+    (assert '(= (length expression) 2))
+    (make-div
+     (derive (first-argument expression) variable)
+     (make-exp (make-cos (first-argument expression)) 2)))
+
+   ((cot? expression)
+    (assert '(= (length expression) 2))
+    (make-sub 0 (make-div
+                 (derive (first-argument expression) variable)
+                 (make-exp (make-sin (first-argument expression)) 2))))
+
    (else
      (print "Unknow expression: " expression))))
 
@@ -223,6 +280,7 @@
   ;; be '((x 2) (y 3)), if no bind value, just pass '()
   ;; in summary algebre-eval provide a more safe way to eval algebre
   ;; expression.
+
   (define (^ base power)
     (cond ((= 0 power) 1)
           ((= 1 power) base)
@@ -230,9 +288,13 @@
           ((even? power) (^ (^ base (/ power 2)) 2))
           (else
             (* base (^ base (- power 1))))))
+
   (define (mylog base up)
     (/ (log up) (log base)))
-  
+
+  (define (cot a)
+    (/ 1 (tan a)))
+
   (cond
    ((constant? expression) expression)
    ((variable? expression)
@@ -242,32 +304,55 @@
             (if (list? env)
               (let ((value (assq expression env)))
                 (if (list? value)
-                  (cadr value)
+                  (algebre-eval (cadr value) env)
+                  ;; so that we can use '((x (/ pi 4))) as env
                   (begin
                    (print expression " is not in env list")
                    (exit 1))))
               (begin
                (print env " is not a list")
                (exit 1))))))
+
    ((add? expression)
     (+ (algebre-eval (first-argument expression) env)
        (algebre-eval (rest-argument expression) env)))
+
    ((sub? expression)
     (if (= (length expression) 2) ;;(- 2)
       (- 0 (algebre-eval (first-argument expression) env))
       (- (algebre-eval (first-argument expression) env)
          (algebre-eval (sub-rest-argument expression) env))))
+
    ((mul? expression)
     (* (algebre-eval (first-argument expression) env)
        (algebre-eval (rest-argument expression) env)))
+
    ((div? expression)
     (/ (algebre-eval (first-argument expression) env)
        (algebre-eval (rest-argument expression) env)))
+
    ((exp? expression)
     (assert '(= (length expression) 3))
     (^ (algebre-eval (first-argument expression) env)
        (algebre-eval (rest-argument expression) env)))
+
    ((log? expression)
     (assert '(= (length expression) 3))
     (mylog (algebre-eval (first-argument expression) env)
-           (algebre-eval (rest-argument expression) env)))))
+           (algebre-eval (rest-argument expression) env)))
+
+   ((sin? expression)
+    (assert '(= (length expression) 2))
+    (sin (algebre-eval (first-argument expression) env)))
+
+   ((cos? expression)
+    (assert '(= (length expression) 2))
+    (cos (algebre-eval (first-argument expression) env)))
+
+   ((tan? expression)
+    (assert '(= (length expression) 2))
+    (tan (algebre-eval (first-argument expression) env)))
+
+   ((cot? expression)
+    (assert '(= (length expression) 2))
+    (cot (algebre-eval (first-argument expression) env)))))
